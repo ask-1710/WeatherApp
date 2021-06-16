68f197843211809a76511512bc463630cc947751
ask-1710 <73017319+ask-1710@users.noreply.github.com>
2021-06-16 16:59:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const argv = require('minimist')(process.argv.slice(2));
 
-let apiKey = '918ede7f348b9717944a2663f7f58958';
+let apiKey = '********************************'; // APIKEY from openweathermap
 let city = argv.c||'portland';  
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
